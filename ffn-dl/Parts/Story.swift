c8f20d4f5be0d311da55ed1628d4267f6130bae6
Alexis Bourget <alexis.bourget@gmail.com>
2020-01-03 00:28:45 +0100

    Added the Story protocol, and its tests.

---
+++ ffn-dl/Parts/Story.swift
@@ -0,0 +1,181 @@
+//
+//  Story.swift
+//  ffn-dl
+//
+//  Created by Alexis Bourget on 2020-01-01.
+//  Copyright © 2020 Alexis Bourget. All rights reserved.
+//
+
+import Foundation
+
+/// A protocol representing a story.
+///
+/// A correctly-built story will always have **at least one valid chapter**.
+///
+/// 
+public protocol Story: CustomStringConvertible {
+  /// URL to the first chapter and/or description of the story.
+  var url: URL { get }
+
+  /// Title of the story.
+  ///
+  /// Should not contains extraneous informations like the author or universe.
+  var title: String { get }
+  /// Summary of the story.
+  var summary: String { get }
+  /// The chapters that make up the story's content.
+  ///
+  /// **Every** story should have at least one chapter and it is an error to allow
+  /// for an empty story to exist.
+  var chapters: [Chapter] { get }
+
+  /// Author of the story.
+  var author: Author { get }
+  /// Universe of the story.
+  var universe: Universe { get }
+  /// Status of the story.
+  var status: Status { get }
+  /// Language of the story.
+  var language: String { get }
+
+  /// Number of chapters in the story.
+  ///
+  /// Default implementation: number of chapters in `self.chapters`
+  var chapterCount: Int { get }
+  /// Number of words in the story.
+  var wordCount: Int { get }
+
+  /// All tokens for the story. Can contains informations like characters, type an so on.
+  var tokens: String { get }
+  /// A shortened version of the tokens, suitable for display in a limited space.
+  ///
+  /// Default implementation: equal to `self.token`.
+  var shortTokens: String { get }
+  /// `true` if the story is complete.
+  ///
+  /// Default implementation: `self.status == .complete`.
+  var isComplete: Bool { get }
+  /// `true` if the story is a crossover.
+  ///
+  /// Default implementation: `self.universe.isCrossover`.
+  var isCrossover: Bool { get }
+
+  /// Last update of the oldest chapter.
+  ///
+  /// Default implementation: find the minimum of all update dates in `self.chapters`.
+  var oldestChapterUpdate: Date { get }
+  /// Most recent update of the oldest chapter.
+  ///
+  /// Default implementation: find the maximum of all update dates in `self.chapters`.
+  var newestChapterUpdate: Date { get }
+  /// Name of the HTML file containing the informations about the story.
+  ///
+  /// Has a default implementation.
+  var HTMLInformationsFilename: String { get }
+
+  /// Indicates whether the story was updated or not, and, if it was, exactly which chapters were.
+  typealias UpdateResult = (state: ffn_dl.UpdateResult, updatedChapters: [Int])
+
+  /// Updates the story, by updating the last known chapter and any new chapter.
+  mutating func update() -> Story.UpdateResult
+
+  /// Updates the given chapters as well as any new chapter.
+  mutating func update(chapters: [Int]) -> Story.UpdateResult
+
+  /// Attempts to initialize a full story fro a given `URL`.
+  /// - Parameters:
+  ///   - url: `URL` to use to get the chapter.
+  init?(from url: URL)
+
+  /// Attempts to initialize a full story fro a given `Document`.
+  /// - Parameters:
+  ///   - doc: `Document` to use to get the chapter.
+  init?(from doc: Document)
+}
+
+public extension Story {
+  // MARK: Computed variables
+
+  var chapterCount: Int {
+    chapters.count
+  }
+
+  var shortTokens: String {
+    tokens
+  }
+
+  var isComplete: Bool {
+    status == .complete
+  }
+
+  var isCrossover: Bool {
+    universe.isCrossover
+  }
+
+  var oldestChapterUpdate: Date {
+    let oldestChapter = chapters.min { $0.lastUpdate < $1.lastUpdate }
+    return oldestChapter!.lastUpdate
+  }
+
+  var newestChapterUpdate: Date {
+    let newestChapter = chapters.max { $0.lastUpdate < $1.lastUpdate }
+    return newestChapter!.lastUpdate
+  }
+
+  var HTMLInformationsFilename: String {
+    let filename = title
+      .lowercased()
+      .replacingOccurrences(of: #"\W+"#, with: "-", options: .regularExpression)
+    return "\(filename)_infos.html"
+  }
+}
+
+public extension Story {
+  // MARK: Update
+
+  mutating func update() -> Story.UpdateResult {
+    update(chapters: [chapterCount - 1])
+  }
+}
+
+public extension Story {
+  // MARK: - Init
+
+  init?(from url: URL) {
+    guard let doc = url.getDocument() else {
+      return nil
+    }
+    self.init(from: doc)
+  }
+}
+
+extension Story {
+  // MARK: - CustomStringConvertible
+
+  public var description: String {
+    let chaptersDesc = chapters
+      .map { "\($0)" }
+      .joined(separator: "\n")
+    return """
+    \(title)
+    URL: \(url.absoluteString)
+    ===============
+    Author: \(author)
+
+    Universe: \(universe)
+
+    Summary: \(summary)
+
+    Language: \(language)
+    Status: \(status)
+
+    Updates:
+    Oldest: \(oldestChapterUpdate.description)
+    Newest: \(newestChapterUpdate.description)
+
+    \(tokens)
+    ---------------
+    \(chaptersDesc)
+    """
+  }
+}
---
+++ ffn-dlTests/PartsTests/StoryTests.swift
@@ -0,0 +1,180 @@
+//
+//  StoryTests.swift
+//  ffn-dlTests
+//
+//  Created by Alexis Bourget on 2020-01-02.
+//  Copyright © 2020 Alexis Bourget. All rights reserved.
+//
+
+import XCTest
+@testable import ffn_dl
+
+// This file only tests the default implementations
+
+// MARK: - Mock Implementations
+
+fileprivate struct MockChapterBuilder {
+  static let url = URL(string: "https://www.fanfiction.net/s/4951074/1/Harry-s-Little-Army-of-Psychos")!
+  static let title = "Test Chapter Title"
+  static let content = "Test chapter content"
+  static let lastUpdate = Date()
+
+  static let finder = Chapter.Finder(
+    findURL: { _ in url },
+    findTitle: { _ in title },
+    findContent: { _ in content }
+  )
+
+  static func build() -> Chapter {
+    Chapter(from: url, withFinder: finder)!
+  }
+}
+
+fileprivate struct MockAuthorBuilder {
+  static let url = URL(string: "https://www.fanfiction.net/u/1122504/RuneWitchSakura")!
+  static let name = "RuneWitchSakura"
+
+  static let finder = Author.Finder(
+    findURL: { _ in url },
+    findName: { _ in name }
+  )
+
+  static func build() -> Author {
+    Author(from: url, withFinder: finder)!
+  }
+}
+
+fileprivate struct MockUniverseBuilder {
+  static let url = URL(string: "https://www.fanfiction.net/book/Harry-Potter/")!
+  static let name = "Harry Potter"
+  static let isCrossover = false
+
+  static let finder = Universe.Finder(
+    findURL: { _ in url },
+    findName: { _ in name },
+    findCrossover: { _ in isCrossover }
+  )
+
+  static func build() -> Universe {
+    Universe(from: url, withFinder: finder)!
+  }
+}
+
+fileprivate struct MockStory: Story {
+  let url = URL(string: "https://www.fanfiction.net/s/4951074/1/Harry-s-Little-Army-of-Psychos")!
+
+  let title = "Test Title"
+  let summary = "Test summary"
+  let chapters = [
+    MockChapterBuilder.build(),
+    MockChapterBuilder.build(),
+    MockChapterBuilder.build()
+  ]
+
+  let author = MockAuthorBuilder.build()
+  let universe = MockUniverseBuilder.build()
+  let status = Status.complete
+  let language = "English"
+
+  let wordCount = 1000
+  let tokens = "Test tokens"
+
+  // Always failing, the parsing of a Document is not the point here
+  init?(from doc: Document) {
+    nil
+  }
+
+  // For successful init
+  init() {}
+
+  func update(chapters: [Int]) -> Story.UpdateResult {
+    (.failure(""), [])
+  }
+}
+
+// MARK: Test class
+
+final class StoryTests: XCTestCase {
+  private let story = MockStory()
+
+  func testChapterCount() {
+    XCTAssertEqual(story.chapterCount, story.chapters.count)
+  }
+
+  func testShortTokens() {
+    XCTAssertEqual(story.shortTokens, story.tokens)
+  }
+
+  func testIsComplete() {
+    XCTAssertTrue(story.isComplete)
+  }
+
+  func testIsCrossover() {
+    XCTAssertEqual(story.isCrossover, story.universe.isCrossover)
+  }
+
+  func testOldestChapterUpdate() {
+    XCTAssertEqual(story.oldestChapterUpdate, story.chapters[0].lastUpdate)
+  }
+
+  func testNewestChapterUpdate() {
+    XCTAssertEqual(story.newestChapterUpdate, story.chapters[2].lastUpdate)
+  }
+
+  func testHTMLInformationsFilename() {
+    XCTAssertEqual(story.HTMLInformationsFilename, "test-title_infos.html")
+  }
+
+  func testDescription() {
+    let desc = """
+    \(story.title)
+    URL: \(story.url.absoluteString)
+    ===============
+    Author: \(story.author)
+
+    Universe: \(story.universe)
+
+    Summary: \(story.summary)
+
+    Language: \(story.language)
+    Status: \(story.status)
+
+    Updates:
+    Oldest: \(story.oldestChapterUpdate.description)
+    Newest: \(story.newestChapterUpdate.description)
+
+    \(story.tokens)
+    ---------------
+    \(story.chapters[0])
+    \(story.chapters[1])
+    \(story.chapters[2])
+    """
+
+    XCTAssertEqual(story.description, desc)
+  }
+
+  func testUpdate() {
+    var story = MockStory()
+    let res = story.update()
+
+    XCTAssertEqual(res.state, UpdateResult.failure(""))
+    XCTAssertEqual(res.updatedChapters, [])
+  }
+
+  func testInitByURLFails() {
+    let url = URL(string: "\(pathToTestDir)/singleChapterFic.html")!
+
+    let story = MockStory(from: url)
+
+    XCTAssertNil(story)
+  }
+
+  func testInitByDocumentFails() {
+    let doc = Document.parse(html: "")!
+
+    let story = MockStory(from: doc)
+
+    XCTAssertNil(story)
+  }
+}
+
