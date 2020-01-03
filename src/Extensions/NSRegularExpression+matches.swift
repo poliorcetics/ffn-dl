e8ac21fccf45faa912e92ee2dcca70f56aacad32
Alexis Bourget <alexis.bourget@gmail.com>
2020-01-03 15:16:52 +0100

    Added an extension to NSRegularExpression in preparation for fanfiction.net struct

---
+++ src/Extensions/NSRegularExpression+matches.swift
@@ -0,0 +1,30 @@
+//
+//  NSRegularExpression+matches.swift
+//  ffn-dl
+//
+//  Created by Alexis Bourget on 2020-01-03.
+//  Copyright © 2020 Alexis Bourget. All rights reserved.
+//
+
+import Foundation
+
+extension NSRegularExpression {
+  /// Attempts to build a regular expression from the given pattern, else raises a `preconditionFailure(_:file:line:)`.
+  /// - Parameter pattern: The regex patterns to use.
+  convenience init(_ pattern: String) {
+    do {
+      try self.init(pattern: pattern)
+    } catch {
+      preconditionFailure("Illegal regular expression: \(pattern).")
+    }
+  }
+
+  /// Returns `true` if the combination of `string` and `options` matches the registered pattern.
+  /// - Parameters:
+  ///   - string: the string to check.
+  ///   - options: the options to use when checking.
+  func matches(_ string: String, options: NSRegularExpression.MatchingOptions = []) -> Bool {
+    let range = NSRange(location: 0, length: string.utf16.count)
+    return firstMatch(in: string, options: options, range: range) != nil
+  }
+}
---
+++ tests/ExtensionsTests/NSRegularExpression+matchesTests.swift
@@ -0,0 +1,79 @@
+//
+//  NSRegularExpression+matchesTests.swift
+//  ffn-dlTests
+//
+//  Created by Alexis Bourget on 2020-01-03.
+//  Copyright © 2020 Alexis Bourget. All rights reserved.
+//
+
+import XCTest
+
+final class NSRegularExpressionTests: XCTestCase {
+  // All tests will be done on those two regexes
+  let oneOrMoreNumbersRegexString = #"^\d+$"#
+  let basicHttpUrlRegexString = #"^(https?://)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(/.*)?$"#
+
+  // Optionals are to ensure the instance of each regex are built before each test
+  // and destroyed just after it, else XCode do not deallocate the memory during the tests
+  var oneOrMoreNumbersRegex: NSRegularExpression!
+  var basicHttpUrlRegex: NSRegularExpression!
+
+  override func setUp() {
+    oneOrMoreNumbersRegex = NSRegularExpression(oneOrMoreNumbersRegexString)
+    basicHttpUrlRegex = NSRegularExpression(basicHttpUrlRegexString)
+  }
+
+  override func tearDown() {
+    oneOrMoreNumbersRegex = nil
+    basicHttpUrlRegex = nil
+  }
+
+  func testConvenienceInit() {
+    let patternMessage = "The patterns should be equal"
+    XCTAssertEqual(oneOrMoreNumbersRegexString, oneOrMoreNumbersRegex!.pattern, patternMessage)
+    XCTAssertEqual(basicHttpUrlRegexString, basicHttpUrlRegex!.pattern, patternMessage)
+  }
+
+  func testMatchWithNoOptionsIsTrue() {
+    let numberMessage = "Numbers should match"
+    XCTAssertTrue(oneOrMoreNumbersRegex.matches("1"), numberMessage)
+    XCTAssertTrue(oneOrMoreNumbersRegex.matches("12"), numberMessage)
+
+    let urlMessage = "Valid URLs should match"
+    XCTAssertTrue(basicHttpUrlRegex.matches("apple.com"), urlMessage)
+    XCTAssertTrue(basicHttpUrlRegex.matches("www.apple.com"), urlMessage)
+
+    XCTAssertTrue(basicHttpUrlRegex.matches("https://apple.com"), urlMessage)
+    XCTAssertTrue(basicHttpUrlRegex.matches("https://apple.com/"), urlMessage)
+    XCTAssertTrue(basicHttpUrlRegex.matches("https://apple.com/support"), urlMessage)
+    XCTAssertTrue(basicHttpUrlRegex.matches("https://www.apple.com/support"), urlMessage)
+
+    XCTAssertTrue(basicHttpUrlRegex.matches("http://apple.com"), urlMessage)
+    XCTAssertTrue(basicHttpUrlRegex.matches("http://apple.com/"), urlMessage)
+    XCTAssertTrue(basicHttpUrlRegex.matches("http://apple.com/support"), urlMessage)
+    XCTAssertTrue(basicHttpUrlRegex.matches("http://www.apple.com/support"), urlMessage)
+  }
+
+  func testMatchWithNoOptionsIsFalse() {
+    let numberMessage = "Non numbers-only should not match"
+    XCTAssertFalse(oneOrMoreNumbersRegex.matches(""), numberMessage)
+    XCTAssertFalse(oneOrMoreNumbersRegex.matches("a"), numberMessage)
+    XCTAssertFalse(oneOrMoreNumbersRegex.matches("1a"), numberMessage)
+    XCTAssertFalse(oneOrMoreNumbersRegex.matches("a1"), numberMessage)
+
+    let urlMessage = "Invalid urls should not match"
+    XCTAssertFalse(basicHttpUrlRegex.matches("apple.c"), urlMessage)
+    XCTAssertFalse(basicHttpUrlRegex.matches("www.apple.c"), urlMessage)
+
+    XCTAssertFalse(basicHttpUrlRegex.matches("https:/apple.com"), urlMessage)
+    XCTAssertFalse(basicHttpUrlRegex.matches("https://apple.c/"), urlMessage)
+    XCTAssertFalse(basicHttpUrlRegex.matches("https//apple.com/support"), urlMessage)
+    XCTAssertFalse(basicHttpUrlRegex.matches("https://www.apple_com/support"), urlMessage)
+
+    XCTAssertFalse(basicHttpUrlRegex.matches("http://apple=com"), urlMessage)
+    XCTAssertFalse(basicHttpUrlRegex.matches("http://Apple.com/"), urlMessage)
+    XCTAssertFalse(basicHttpUrlRegex.matches("http://-pple.com/support"), urlMessage)
+    XCTAssertFalse(basicHttpUrlRegex.matches("htp://www.apple.com/support"), urlMessage)
+  }
+}
+
