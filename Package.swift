33c476dcfc7da51b25f5246e03e1171d7f2720fa
Alexis Bourget <alexis.bourget@gmail.com>
2020-01-04 23:39:39 +0100

    Updated Package.swift to comply with SPM rules

--- Package.swift
+++ Package.swift
@@ -1,4 +1,12 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1.0
+
+//
+//  Package.swift
+//
+//  Created by Alexis Bourget on 2019-01-04.
+//  Copyright © 2019 Alexis Bourget. All rights reserved.
+//
+
 import PackageDescription
 
 let package = Package(
@@ -10,7 +18,7 @@ let package = Package(
     .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.0"),
   ],
   targets: [
-    .target(name: "ffn-dl", dependencies: ["SwiftSoup"]),
-    .target(name: "ffn-dlTests", dependencies: ["ffn-dl"]),
+    .target(name: "ffn-dl", dependencies: ["SwiftSoup"], path: "Sources"),
+    .testTarget(name: "ffn-dlTests", dependencies: ["ffn-dl"], path: "Tests"),
   ]
 )
